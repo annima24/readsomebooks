d350040ca6d7f8bb65910730904d3d384bbab635
annima24 <nm.andrew24@gmail.com>
2021-04-13 16:31:19 -0400

    performs checks to make sure fields are properly filled out, will start styling soon

--- script.js
+++ script.js
@@ -7,6 +7,7 @@ const submitBtn = document.querySelector('#submitBtn');
 const entryForm = document.querySelector('#entryForm');
 const addBookBtn = document.querySelector('#addBook');
 let myLibrary = [];
+let checked = 'true';
 
 function Book(title, author, pages, hasRead) {
     this.title = title
@@ -19,6 +20,9 @@ function addBookToLibrary(book) {
     myLibrary.push(book);
 }
 
+function fillEmptyFieldAlert()  {
+    alert('please fill in empty fields');
+}
 function deleteBook(removeBtn)   {
     myLibrary.splice(removeBtn.dataset.remove, 1), displayBooks()
 }
@@ -29,19 +33,19 @@ function displayForm()  {
         entryForm.style.display = ''
     } 
 }
-
+//the function that handles the creation of the cards on click after a check to make sure fields are properly filled out.
 function createBookCard(book, i) {
-    
+    //creating the elements of the card
     const para = document.createElement('p');
     const readBtn = document.createElement('button');
     const removeBtn = document.createElement('button');
-    
+    //setting attributs and text
     para.setAttribute('data-set', i)
     para.innerText = `Title: ${book.title}  
     Author: ${book.author} 
     Pages: ${book.pages}        
     Completed: ${book.hasRead}`;
-    
+    //setting attributes and putting event listeners on the btns. this controls the state of the hasread part of the object
     readBtn.setAttribute('class', 'read')
     readBtn.textContent = 'read';
     readBtn.setAttribute('data-read', i)
@@ -51,25 +55,33 @@ function createBookCard(book, i) {
         } else if (myLibrary[readBtn.dataset.read].hasRead === 'No')  {
             myLibrary[readBtn.dataset.read].hasRead = 'Yes', displayBooks();
     }})
-    
+    //setting attributes and putting event listeners on the btns. this removes the object from the array if the btn is clicked and then returns the new array
     removeBtn.textContent = 'remove';
     removeBtn.setAttribute('class', 'remove')
     removeBtn.setAttribute('data-remove', i)
     removeBtn.addEventListener('click', function()  {
         myLibrary.splice(removeBtn.dataset.remove, 1), displayBooks()
     });
-
+    //adding the cards to the display
     output.appendChild(para);
     para.appendChild(readBtn)
-    para.appendChild(removeBtn)
-    
+    para.appendChild(removeBtn)    
 }
 
+//loops through the array that holds all the books and creates display cards for each.
 function displayBooks() {
     output.textContent = '';
     myLibrary.forEach((book, i)=> createBookCard(book, i));
 }
 
+function checkFields()  {
+    if (titleInput.value === '' || authorInput.value === '')    {
+        fillEmptyFieldAlert();
+    } else if (isNaN(parseInt(pagesInput.value)) === true)    {
+        alert('numbers only please');
+    } else createNewObject();
+ }
+ 
 function clearFields()  {
     titleInput.value = '';
     authorInput.value = '';
@@ -78,17 +90,15 @@ function clearFields()  {
 //when the submit button is clicked, this takes all the values and puts them into an object, it then pushes that object to the myLibrary array, then updates the view field with the new book being displayed and clears out the form.
 function createNewObject()   {
     let read;
-    radioInput.forEach(btn => {
-        if(btn.checked)    {
-            read = btn.value
-        }});
-        const newBook = new Book(titleInput.value,authorInput.value,pagesInput.value, read);
-        addBookToLibrary(newBook);
+    radioInput.forEach(btn => read = btn.value);
+    const newBook = new Book(titleInput.value,authorInput.value,pagesInput.value, read);
+    addBookToLibrary(newBook);
     displayBooks();
     clearFields();
+    
 }
 
-submitBtn.addEventListener('click' , createNewObject);
+submitBtn.addEventListener('click' , checkFields);
 addBookBtn.addEventListener('click', displayForm)
 
 const bookOne = new Book('The Hobbit', 'Tolken', 298, 'Yes');
@@ -99,7 +109,5 @@ displayBooks();
 // const removeBtns = document.querySelectorAll('.remove').forEach(btn => btn.addEventListener('click', function() {
 //     myLibrary.splice(btn.dataset.remove, 1), displayBooks();
 // }))
-
-
 
 
